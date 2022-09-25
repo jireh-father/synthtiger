32c0afadf5dc9ddb2d2175f4faf0a04116a0cfe6
seoilgun <seoilgun@gmail.com>
2022-09-25 15:58:56 +0900

    g

--- examples/synthtable/utils/html_style.py
+++ examples/synthtable/utils/html_style.py
@@ -7,12 +7,8 @@ def add_styles(target_styles, new_styles):
             target_styles[selector][style_key] = new_styles[selector][style_key]
 
 
-def parse_html_style_values_list(list_values):
-    list(range(list_values[0], list_values[1] + 1)), [1] * len(list_values)
-
-
 def parse_html_style_values_dict(dict_values):
-    weights = dict_values['weights'] if 'weights' in dict_values else [1] * len(dict_values['values'])
+    weights = dict_values['weights'] if 'weights' in dict_values else None
     postfix = dict_values['postfix'] if 'postfix' in dict_values else None
 
     return dict_values['values'], weights, postfix
@@ -20,10 +16,7 @@ def parse_html_style_values_dict(dict_values):
 
 def parse_html_style_values(values):
     if isinstance(values, list):
-        if len(values) == 2 and all(isinstance(v, int) for v in values):
-            return Selector(*parse_html_style_values_list(values))
-        else:
-            return Selector(values, [1] * len(values))
+        return Selector(values)
     elif isinstance(values, dict):
         return Selector(*parse_html_style_values_dict(values))
 
