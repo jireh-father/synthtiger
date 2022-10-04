3a0bd8f2c72a30bcbecb7cb2040476dabd67507d
seoilgun <seoilgun@gmail.com>
2022-10-04 17:17:54 +0900

    s

--- examples/synthtable/components/corpus/table_corpus.py
+++ examples/synthtable/components/corpus/table_corpus.py
@@ -106,8 +106,9 @@ class TableCorpus(Component):
         self._contents_tbody = []
         self._offsets_tbody = []
         self._counts_tbody = []
-
+        print(self.paths)
         for path in self.paths:
+            print(path)
             offset_thead = 0
             count_thead = 0
             contents_thead = io.StringIO()
@@ -161,6 +162,9 @@ class TableCorpus(Component):
             self._contents_tbody.append(contents_tbody.getvalue())
             self._offsets_tbody.append(np.frombuffer(offsets_tbody.getvalue(), dtype=np.uint32))
             self._counts_tbody.append(count_tbody)
+            print(self._counts_thead)
+            print(self._counts_tbody)
+            sys.exit()
 
             contents_thead.close()
             offsets_thead.close()
