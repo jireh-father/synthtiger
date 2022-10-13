fbf1f23f7d9dfff3adc9440ed1b305961638e712
seoilgun <seoilgun@gmail.com>
2022-10-13 19:18:59 +0900

    fsdf

--- examples/synthtable/layers/table_layer.py
+++ examples/synthtable/layers/table_layer.py
@@ -89,7 +89,7 @@ class TableLayer(Layer):
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         driver = webdriver.Chrome('chromedriver', options=options)
-        driver.implicitly_wait(2)
+        driver.implicitly_wait(0.5)
 
         self._write_html_file(html_path)
         driver.get("file:///{}".format(os.path.abspath(html_path)))
