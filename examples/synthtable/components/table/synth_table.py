e511e279911a3c6df6f1f4c25478e8f05f5e4af5
seoilgun <seoilgun@gmail.com>
2022-10-13 18:52:22 +0900

    s

--- examples/synthtable/components/table/synth_table.py
+++ examples/synthtable/components/table/synth_table.py
@@ -575,16 +575,16 @@ class SynthTable(Component):
                     self._sample_font(css_selector_name)
 
             # td: text vertical
-            if config_key == "td" and absolute_config['text_vertical'].on():
-                max_text_length = absolute_config['text_vertical'].get()[
-                    "max_text_length"].select()
-                ignore_number = absolute_config['text_vertical'].get()[
-                    "ignore_number"].on()
-
-                if 1 < len(bs_element.text) <= max_text_length and (
-                        ignore_number and any(not c.isdigit() for c in bs_element.text)):
-                    self.global_style[css_selector_name]['text-orientation'] = 'upright'
-                    self.global_style[css_selector_name]['writing-mode'] = 'vertical-rl'
+            # if config_key == "td" and absolute_config['text_vertical'].on():
+            #     max_text_length = absolute_config['text_vertical'].get()[
+            #         "max_text_length"].select()
+            #     ignore_number = absolute_config['text_vertical'].get()[
+            #         "ignore_number"].on()
+            #
+            #     if 1 < len(bs_element.text) <= max_text_length and (
+            #             ignore_number and any(not c.isdigit() for c in bs_element.text)):
+            #         self.global_style[css_selector_name]['text-orientation'] = 'upright'
+            #         self.global_style[css_selector_name]['writing-mode'] = 'vertical-rl'
 
             # relative
             if 'font_size' in local_config['relative'][config_key]:
